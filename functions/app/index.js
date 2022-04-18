6461dcb18aa06e10733c6a51068733c59cc86a19
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-04-18 11:21:49 +0900

    Update index.js

--- functions/app/index.js
+++ functions/app/index.js
@@ -1,17 +1,30 @@
 import express from 'express'
+import path from 'path'
+import fs from 'fs'
+import ytdl from 'ytdl-core'
 import cors from 'cors'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import compression from 'compression'
+import { exec } from 'child_process';
 import customLogger from '../utils/logger'
 
 /* My express App */
 export default function expressApp(functionName) {
   const app = express()
   const router = express.Router()
+  fs.mkdir(path.resolve(__dirname, './tmp/'), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+
 
   // gzip responses
   router.use(compression())
+  router.use(express.json());
+  router.use(express.urlencoded({ extended: true }));
+  router.use(express.static(path.resolve(__dirname, 'static')));
 
   // Set router base path for local dev
   const routerBasePath = process.env.NODE_ENV === 'dev' ? `/${functionName}` : `/.netlify/functions/${functionName}/`
@@ -57,18 +70,46 @@ export default function expressApp(functionName) {
     res.send(html)
   })
 
-  router.get('/users', (req, res) => {
-    res.json({
-      users: [
-        {
-          name: 'steve',
-        },
-        {
-          name: 'joe',
-        },
-      ],
-    })
-  })
+  router..get('/api/youtube/:youtubeId', (req, res) => {
+  const { youtubeId } = req.params;
+  const fileType = (req.query.fileType || 'mp4') as 'mp4' | 'mp3';
+
+  const destFilePath = path.resolve(__dirname, `./tmp/${youtubeId}.mp4`);
+
+  const url = `https://www.youtube.com/watch?v=${youtubeId}`;
+  const stream = ytdl(url, { quality: 'highest' });
+
+  stream.pipe(fs.createWriteStream(destFilePath));
+
+  stream.on('error', (err) => {
+    console.error(err);
+    res.status(400).send('download error!');
+  });
+
+  stream.on('end', () => {
+    console.log(`youtube file (${youtubeId}.mp4) downloaded.`);
+
+    // mp4の場合はそのまま返す
+    if (fileType === 'mp4') {
+      res.download(destFilePath);
+      return;
+    }
+
+    // mp3の場合は変換してから返す
+    console.log('transform mp4 -> mp3.');
+    const mp3FilePath = path.resolve(__dirname, `./tmp/${youtubeId}.mp3`);
+    exec(`ffmpeg -y -i ${destFilePath} ${mp3FilePath}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('movie translation error!');
+        return;
+      }
+      console.log(stdout);
+      console.log(stderr);
+      res.download(mp3FilePath);
+    });
+  });
+});
 
   router.get('/hello/', function(req, res) {
     res.send('hello world')
