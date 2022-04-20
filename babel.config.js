f6d5f9a5f8d206e2f97a7bc993536e7bebcfffb1
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-04-20 10:07:07 +0900

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ const presets =  [
     "modules": false,
     "targets": [">0.25% in JP", "not ie <= 10", "not op_mini all"],
     "useBuiltIns": "usage",
+    "corejs": 3
   }]
 ];
 module.exports = { presets }
