c9173b6f7df21f029a753b64050b564740521b5b
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-09-15 18:49:16 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,7 @@ export default new Router({
       path: "/youtube",
       component: YoutubePlayer
     },{
-      path: "/video/:id",
+      path: "/video",
       component: VideoPage
     },{
       path: "*",
