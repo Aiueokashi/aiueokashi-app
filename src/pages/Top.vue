15f32dfce3fecb20e1c4369bd3956ce26952868e
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-09-15 18:34:47 +0900

    Update Top.vue

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -31,7 +31,7 @@ export default {
           description: "製作中のプロジェクトです。まあ、ようつべから動画とってくるアレです。",
           icon: "logo-youtube",
           insite: true,
-          link: "/ytdl",
+          link: "/video/a",
           linkmsg: "Go to Youtube Downloader"
       },{
           number:2,
