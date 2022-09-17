d3a910ce1055267f603b9f4a82b0f8e5564e983a
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-09-17 19:33:41 +0900

    Update VideoPlayer.vue

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -43,8 +43,8 @@
       <div v-if="showFunctions" class="video__controls__functions">
         <div>
           <button class="video__controls__button" @click.stop="toggleVideoPlay">
-            <v-icon style="color: #fff" v-if="isPlaying"><ion-icon size="large" class="pause-icon" name="pause-circle-outline"></ion-icon></v-icon>
-            <v-icon style="color: #fff" v-else><ion-icon size="large" class="play-icon" name="play-circle-outline"></ion-icon></v-icon>
+            <ion-icon v-if="isPlaying" size="large" class="pause-icon" name="pause-circle-outline"></ion-icon>
+            <ion-icon v-else size="large" class="play-icon" name="play-circle-outline"></ion-icon>
           </button>
 
           <div class="video__controls__duration">
