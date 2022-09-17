4caa0e44c563ba1cff2fa68ae24f72d09e1496cb
AiueoKashi <70052601+Aiueokashi@users.noreply.github.com>
2022-09-17 19:22:56 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
+    <transition mode=”out-in”>
     <router-view />
+    </transition>
     <Footer />
   </div>
 </template>
@@ -36,5 +38,26 @@ body {
   text-align: center;
   color: #2c3e50;
   background-color: rgb(54, 51, 51);
+}
+.v-enter {
+  transform: translate(-100px, 0);
+  opacity: 0;
+}
+.v-enter-to {
+  opacity: 1;
+}
+.v-enter-active {
+  transition: all 1s 0s ease;
+}
+.v-leave {
+  transform: translate(0, 0);
+  opacity: 1;
+}
+.v-leave-to {
+  transform: translate(100px, 0);
+  opacity: 0;
+}
+.v-leave-active {
+  transition: all .5s 0s ease;
 }
 </style>
